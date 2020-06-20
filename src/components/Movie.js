4c62ac7947375820cdb7633b068d9c1fa5ea3ff3
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-06-20 18:13:02 +0100

    ..

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,27 +1,37 @@
 import React from "react";
+import {useState} from 'react';
+
+
+
 
 const DEFAULT_PLACEHOLDER_IMAGE =
   "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
 
 
 const Movie = ({ movie }) => {
+  const [onHover , setHoverState] = useState(false);
+  let classList = ['movie_wrapper'];
+  classList = onHover ? ['movie_wrapper_overlay'] : ['movie_wrapper'];
   const poster =
     movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
-  return (
-    <div className="movie">
-      <div className="movie_wrapper">
-        <img
-          width="200"
-          alt={`The movie titled: ${movie.Title}`}
-          src={poster}
-        />
+    return (
+      <div className="movie" onMouseEnter={() => setHoverState(true)} onMouseLeave={() => setHoverState(false)}>
+        <div className={classList}>
+          <img
+            width="200"
+            alt={`The movie titled: ${movie.Title}`}
+            src={poster}
+          />
+        </div>
+        <div className="movie__overlay_content">
+          
+        </div>
+        <h5 className="movie__title_tag">{movie.Title} <br />
+          <span style={{color: "grey", fontSize: '16px', textAlign:'left'}}>({movie.Year})</span>
+        </h5>
+        
       </div>
-      <h5 className="movie__title_tag">{movie.Title} <br />
-        <span style={{color: "grey", fontSize: '16px', textAlign:'left'}}>({movie.Year})</span>
-      </h5>
-      
-    </div>
-  );
+    );
 };
 
 
