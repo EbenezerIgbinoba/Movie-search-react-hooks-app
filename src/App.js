d171812c128a3041676706a31e2434b7989d1113
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-06-20 17:36:56 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import React from 'react';
-import Layout from  './Components/AppLayout';
 import Banner from './Components/Banner';
 import Movies from './Components/Movies';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      textToBeFiltered: ''
-    };
-  }
-  filterMovies = (filtertext) => {
-    this.setState({textToBeFiltered: filtertext});
-  }
-  render() {
+const  App = (props) => {
     return (
-      <Layout filtertext={(filtertext) => this.filterMovies(filtertext)}>
-         <div style={{
-             backgroundImage: `linear-gradient(rgb(0, 0, 0 , 0.86), rgb(0, 0, 0 , 0.86)), url('https://img.yts.mx/assets/images/movies/onward_2020/background.jpg')`,
-             backgroundPosition: 'center',
-             backgroundRepeat: 'no-repeat',
-             backgroundSize: 'cover'
-          }}>
-            <Banner />
-            <Movies key={this.state.textToBeFiltered}  text={this.state.textToBeFiltered}    />
-          </div>
-      </Layout>
+      <div style={{
+          backgroundImage: `linear-gradient(rgb(0, 0, 0 , 0.86), rgb(0, 0, 0 , 0.86)), url('https://img.yts.mx/assets/images/movies/onward_2020/background.jpg')`,
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover'
+      }}>
+        <Banner />
+        <Movies key={props.filerText}  text={props.filerText}    />
+      </div>
     )
-  }
 }
-
-
-
 
 export default App;
--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,31 @@
 import {Route, Switch} from 'react-router-dom';
 import App from './App';
 import React from 'react';
+import Layout from './Components/AppLayout'
 
-const Routes = () => {
-    return (
-        <div>
-            <Switch>
-                <Route exact path="/" component={App} />
-            </Switch>
-        </div>
-    )
+
+class Routes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            textToBeFiltered: ''
+        };
+    }
+    filterMovies = (filtertext) => {
+        this.setState({textToBeFiltered: filtertext});
+    }
+
+    render() {
+        return (
+            <Layout filtertext={(filtertext) => this.filterMovies(filtertext)}>
+                <Switch>
+                    <Route exact path="/"  render={(props) => (<App  filerText={this.state.textToBeFiltered} /> )} />
+                    <Route exact path="/movies/details"   />
+                </Switch>
+            </Layout>
+        )
+    }
+ 
 }
 
 export default  Routes;
