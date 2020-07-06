d0fa28c929ab046778f013b65692fe3aaa7b7a32
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-07-07 00:28:30 +0100

    Login and Register Component

--- src/components/utils/Login_Register_Modal.js
+++ src/components/utils/Login_Register_Modal.js
@@ -3,6 +3,9 @@ import {useEffect, useState, useRef} from 'react';
 
 const Modal = ({ modalOpen, hideModal, tab}) => {
     const modal_main_ref = useRef();
+    const emailRef = useRef();
+    const usernameRef = useRef();
+    const passRef = useRef();
     const [activeTab, activateTab] = useState('login'); 
 
     useEffect(() => {
@@ -13,6 +16,51 @@ const Modal = ({ modalOpen, hideModal, tab}) => {
         };
       }, []);
 
+    const validateEmail = email =>  {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+          return true;
+        }
+        return false;
+    }
+    const validatePasword = password => {
+        if (password.length > 6) {
+          return true;
+        }
+        return false;
+    }
+    const validateInput = e => {
+        let _errNode = e.target.parentNode.children[1];
+        if(e.target.name === 'email') {
+            if(!validateEmail(e.target.value)) {
+                emailRef.current.style.border = '1px solid red';
+                _errNode.innerHTML = 'Invalid Email format';
+            }else {
+                emailRef.current.style.border = '1px solid #d2d8d8';
+                _errNode.innerHTML = '';
+            }
+        }
+        if(e.target.name === 'username') {
+            if(e.target.value === "") {
+                usernameRef.current.style.border = '1px solid red';
+                _errNode.innerHTML = 'This Field iws required';
+            }else {
+                usernameRef.current.style.border = '1px solid #d2d8d8';
+                _errNode.innerHTML = '';
+            }
+        }
+        if(e.target.name === 'password') {
+            if(e.target.value === "") {
+                passRef.current.style.border = '1px solid red';
+                _errNode.innerHTML = 'Fill in this field';
+            }else if(!validatePasword(e.target.value)) {
+                passRef.current.style.border = '1px solid red';
+                _errNode.innerHTML = 'Password must be more than six(6) characters';
+            }else {
+                passRef.current.style.border = '1px solid #d2d8d8';
+                _errNode.innerHTML = '';
+            }
+        }
+    }
     const handleClick = e => {
         if (modal_main_ref.current.contains(e.target)) {
             return;
@@ -41,10 +89,11 @@ const Modal = ({ modalOpen, hideModal, tab}) => {
                            <div>
                                 <div className="input_wrapper">
                                     <div className="input_icon_placeholder">
-                                        <i className="fa fa-user" aria-hidden="true" style={{color: '#eaeaea', fontSize: '20px'}}></i>
+                                        <i className="fa fa-user" name="username" aria-hidden="true" style={{color: '#eaeaea', fontSize: '20px'}}></i>
                                     </div>
                                     <div className="input_div">
-                                        <input type="text" placeholder="Username or Email" className="input__container"  />
+                                        <input type="email" placeholder="Username or Email" name="email" className="input__container"  />
+                                        <div className="error_flag" ></div>
                                     </div>
                                 </div>
                                 <div className="input_wrapper mt-4">
@@ -64,28 +113,33 @@ const Modal = ({ modalOpen, hideModal, tab}) => {
                         ) : 
                         (
                             <div>
-                                <div className="input_wrapper">
+                                <div className="input_wrapper" ref={usernameRef}>
                                     <div className="input_icon_placeholder">
                                         <i className="fa fa-user" aria-hidden="true" style={{color: '#eaeaea', fontSize: '20px'}}></i>
                                     </div>
                                     <div className="input_div">
-                                        <input type="text" placeholder="Username" className="input__container"  />
+                                        <input type="text" placeholder="Username" name="username" className="input__container" onBlur={(e) => validateInput(e)} />
+                                        <div className="error_div"></div>
                                     </div>
+                                    
                                 </div>
-                                <div className="input_wrapper mt-4">
+                                <div className="input_wrapper mt-4" ref={emailRef} >
                                     <div className="input_icon_placeholder">
                                         <i className="fa fa-envelope" aria-hidden="true" style={{color: '#eaeaea', fontSize: '20px'}}></i>
                                     </div>
-                                    <div className="input_div">
-                                        <input type="email" placeholder="Email" className="input__container"  />
+                                    <div className="input_div" >
+                                        <input type="email" placeholder="Email" name="email" className="input__container" onBlur={(e) => validateInput(e)}   />
+                                        <div className="error_div"></div>
                                     </div>
+                                    
                                 </div>
-                                <div className="input_wrapper mt-4">
+                                <div className="input_wrapper mt-4" ref={passRef}>
                                     <div className="input_icon_placeholder">
                                         <i className="fa fa-lock" aria-hidden="true" style={{color: '#eaeaea', fontSize: '20px'}}></i>
                                     </div>
                                     <div className="input_div">
-                                        <input type="password" placeholder="Password" className="input__container"  />
+                                        <input type="password" name="password" placeholder="Password" className="input__container" onBlur={(e) => validateInput(e)} />
+                                        <div className="error_div"></div>
                                     </div>
                                 </div>
                                 <div className="input_wrapper mt-4">
